36f18e9037907d3d2e0d5a1ec4edd890f0873937
NoahSch1999 <77342033+NoahSch1999@users.noreply.github.com>
2023-05-22 16:54:23 +0200

    Final update of repo

--- oldengine/aZeroEngine/D3D12Include.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "directx/d3dx12.h"
-#include <d3dcompiler.h>
-#include <dxgi1_3.h>
-#include <wrl.h>
-
-using namespace Microsoft::WRL;
--- oldengine/aZeroEngine/PS_Outline.hlsl
+++
@@ -1,9 +0,0 @@
-cbuffer colorConstant : register(b0)
-{
-    float4 color;
-}
-
-float4 main(float4 position : SV_Position) : SV_TARGET
-{
-    return color;
-}
